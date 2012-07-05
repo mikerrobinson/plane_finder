fdf50d07ad40f7d8ad269aea507a3a5c5aa4248c
Mike Robinson <mikerrobinson@hotmail.com>
2012-07-05 15:05:32 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlaneFinder::Application.config.secret_token = '7650a2910c69c723593747a696dc23e0e455da0c75d094b7504df550aaf2670901cc923c3023ca23723c65a122565b7736237ad3191d965d62b1023b8c10ade1'
