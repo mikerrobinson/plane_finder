9def87361f8ce4033f35d18cdd8b9858201a74c2
Mike Robinson <mikerrobinson@hotmail.com>
2012-07-06 07:20:52 -0700

    base site per tutorial complete

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,46 +2,42 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
 
-    it "should have the h1 'Plane Renter'" do
-      visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Plane Renter')
-    end
-
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text => "Plane Renter | Home")
-    end
+    before { visit root_path }
+  
+    it { should have_selector('h1', :text => 'Plane Renter') }
+    it { should have_selector('title', :text => "Plane Renter | Home") }
     
   end
   
   describe "Help page" do
 
-    it "should have the h1 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
-    end
+    before { visit help_path }
 
-    it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text => "Plane Renter | Help")
-    end
+    it { should have_selector('h1', :text => 'Help') }
+    it { should have_selector('title', :text => "Plane Renter | Help") }
 
   end
   
   describe "About page" do
 
-    it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
-      page.should have_selector('h1', :text => 'About Us')
-    end
-
-    it "should have the title 'About'" do
-      visit '/static_pages/about'
-      page.should have_selector('title', :text => "Plane Renter | About")
-    end
+    before { visit about_path }    
+    
+    it { should have_selector('h1', :text => 'About Us') }
+    it { should have_selector('title', :text => "Plane Renter | About") }
 
   end
   
+  describe "Contact page" do
+
+    before { visit contact_path }
+    
+    it { should have_selector('h1', :text => 'Contact') }
+    it { should have_selector('title', :text => "Plane Renter | Contact") }
+
+  end
+    
 end
