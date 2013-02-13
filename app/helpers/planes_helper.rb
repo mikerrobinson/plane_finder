943a392543478fed08b95bc26a19dde384cb3cbb
Michael Robinson <mrobinson@Michaels-MacBook-Air.local>
2013-02-13 10:40:31 -0700

    add helper for unlisted rental rates

--- app/helpers/planes_helper.rb
+++ app/helpers/planes_helper.rb
@@ -7,5 +7,10 @@ module PlanesHelper
   def formatted_cost(plane)
     "$#{sprintf("%5.0f",plane.rental_amount)}/hr #{plane.rental_type}"
   end
-    
+
+  def rental_text(rental_amount)
+    return "$#{rental_amount.to_i}/hr" if rental_amount.to_i > 0
+    return I18n.t("views.planes.rental_amount_na")
+  end
+  
 end
